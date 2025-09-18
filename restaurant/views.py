eb1655204bb01be206f5af0ff602964d4b3aaabe
Andrei <aalexandru518@gmail.com>
2025-09-18 18:06:03 +0100

    Created database, added connection settings & created Booking & Menu models

--- restaurant/views.py
+++ restaurant/views.py
@@ -1,8 +1,5 @@
 from django.shortcuts import render
 
 # Create your views here.
-<<<<<<< HEAD
-=======
 def home(request):
     return render(request, 'index.html', {})
->>>>>>> 0431b6d (Added index.html and a basic view)
