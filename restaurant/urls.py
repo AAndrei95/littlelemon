7c7755f5eded3586f414193bb556ec92acd41ca9
Andrei <aalexandru518@gmail.com>
2025-09-21 09:33:00 +0100

    Added token authentication

--- restaurant/urls.py
+++ restaurant/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path, include
+from rest_framework.authtoken.views import obtain_auth_token
 from rest_framework.routers import DefaultRouter
 from . import views 
 
 urlpatterns = [
     path('menu/', views.MenuItemView.as_view()),
     path('menu/<int:pk>', views.SingleItemView.as_view()),
+    path('api-token-auth/', obtain_auth_token),
 ]
--- restaurant/views.py
+++ restaurant/views.py
@@ -1,19 +1,23 @@
 from django.shortcuts import render
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.generics import ListCreateAPIView, RetrieveUpdateAPIView, DestroyAPIView
 from rest_framework.viewsets import ModelViewSet
 from .models import Menu, Booking
 from .serializers import MenuSerializer, BookingSerializer
 
 class MenuItemView(ListCreateAPIView):
+    permission_classes = [IsAuthenticated]
     queryset = Menu.objects.all()
     serializer_class = MenuSerializer  
 
 
 class SingleItemView(RetrieveUpdateAPIView, DestroyAPIView):
+    permission_classes = [IsAuthenticated]
     queryset = Menu.objects.all()
     serializer_class = MenuSerializer
 
 
 class BookingViewSet(ModelViewSet):
+    permission_classes = [IsAuthenticated]
     queryset = Booking.objects.all()
     serializer_class = BookingSerializer
