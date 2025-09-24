ebb102c4927053d6f721408f1c42467ba5583ac4
Andrei <aalexandru518@gmail.com>
2025-09-24 10:50:15 +0100

    Added home & refactor MenuView test case

--- restaurant/urls.py
+++ restaurant/urls.py
@@ -1,9 +1,9 @@
-from django.urls import path, include
+from django.urls import path
 from rest_framework.authtoken.views import obtain_auth_token
-from rest_framework.routers import DefaultRouter
 from . import views 
 
 urlpatterns = [
+    path('home', views.home, name='home'),
     path('menu/', views.MenuItemView.as_view()),
     path('menu/<int:pk>', views.SingleItemView.as_view()),
     path('api-token-auth/', obtain_auth_token),
--- tests/test_views.py
+++ tests/test_views.py
@@ -1,11 +1,18 @@
+from django.contrib.auth.models import User
 from django.test import TestCase
 from django.urls import reverse
 from rest_framework import status
 from restaurant.models import Menu
 from restaurant.serializers import MenuSerializer
+from rest_framework.test import APIClient, APITestCase
 
 class MenuViewTest(TestCase):
     def setUp(self):
+        self.user = User.objects.create_user(username='testuser', password='password123!')
+        
+        self.client = APIClient()
+        self.client.force_authenticate(user=self.user)
+        
         self.menu1 = Menu.objects.create(title='Fried Calamari', price=6.50, inventory=20)
         self.menu2 = Menu.objects.create(title='Tiramisu', price=4.50, inventory=10)
         self.menu3 = Menu.objects.create(title='Grilled Fish', price=12.50, inventory=30) 
@@ -17,4 +24,3 @@ class MenuViewTest(TestCase):
         serializer = MenuSerializer(menu, many=True)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(response.data, serializer.data)
-        
