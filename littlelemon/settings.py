c65e6927a2ba1ebb3f4294796b3ff18654324e86
Andrei <aalexandru518@gmail.com>
2025-09-24 11:10:16 +0100

    Added README

--- littlelemon/settings.py
+++ littlelemon/settings.py
@@ -81,8 +81,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'littlelemon_db',
-        'USER': 'root',
-        'PASSWORD': 'Hustlero1!',
+        'USER': 'admindjango',
+        'PASSWORD': 'employee@123!',
         'HOST': '127.0.0.1',
         'PORT': '3306',
         'OPTIONS': {
